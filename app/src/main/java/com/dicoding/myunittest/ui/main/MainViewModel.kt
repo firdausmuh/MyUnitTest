2edbb363cfa5f276901df0238ee85ccbd973eddb
muhfrds345@gmail.com <muhfrds345@gmail.com>
2023-09-24 03:19:13 +0700

    Initial commit testing

---
+++ app/src/main/java/com/dicoding/myunittest/ui/main/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.dicoding.myunittest.ui.main
+
+class MainViewModel(private val cuboidModel: CuboidModel) {
+
+    fun getCircumference() = cuboidModel.getCircumference()
+
+    fun getSurfaceArea() = cuboidModel.getSurfaceArea()
+
+    fun getVolume() = cuboidModel.getVolume()
+
+    fun save(w: Double, l: Double, h: Double) {
+        cuboidModel.save(w, l, h)
+    }
+}
